20dbabd76639e2a69441292349211aea64005522
K0ras1K <romakalmykov@gmail.com>
2022-05-12 15:37:30 +0300

    Initial Commit

--- src/main/java/k0ras1k/reg/blocks/BlockList.java
+++ src/main/java/k0ras1k/reg/blocks/BlockList.java
@@ -1,4 +1,21 @@
 package k0ras1k.reg.blocks;
 
+import k0ras1k.blocks.BlockBigFurnance;
+import k0ras1k.blocks.BlockNeutronBig;
+import k0ras1k.blocks.BlockTransformer;
+import k0ras1k.blocks.solarPanels.candyPanels.BlockCandyPanel;
+import net.minecraft.block.Block;
+
 public class BlockList {
+
+    public static Block candy_panel_blue = new BlockCandyPanel("candy_panel_blue", 100000, 50000, 1000000, 100000, "blue").setBlockName("candy_panel_blue");
+    public static Block candy_panel_yellow = new BlockCandyPanel("candy_panel_yellow", 300000, 150000, 10000000, 1000000, "yellow").setBlockName("candy_panel_yellow");
+    public static Block candy_panel_red = new BlockCandyPanel("candy_panel_red", 1000000, 500000, 100000000, 10000000, "red").setBlockName("candy_panel_red");
+    public static Block candy_panel_green = new BlockCandyPanel("candy_panel_green", 10000000, 5000000, 1000000000, 100000000, "green").setBlockName("candy_panel_green");
+    public static Block candy_panel_cloud = new BlockCandyPanel("candy_panel_cloud", 100000000, 50000000, 1000000000, 1000000000, "cloud").setBlockName("candy_panel_cloud");
+    public static Block transformer = new BlockTransformer().setBlockName("transformer");
+    public static Block neutron_collector_big = new BlockNeutronBig().setBlockName("neutron_collector_big");
+    public static Block big_furnance = new BlockBigFurnance().setBlockName("big_furnance");
+
+
 }
---
+++ src/main/java/k0ras1k/reg/items/ItemList.java
@@ -0,0 +1,12 @@
+package k0ras1k.reg.items;
+
+import k0ras1k.items.ItemNeutronUpgrade;
+import net.minecraft.item.Item;
+
+public class ItemList {
+
+    public static Item ItemNeutronUpgradeSpeed = new ItemNeutronUpgrade("ItemsNeutronUpgrade", "ItemsNeutronUpgrade");
+    public static Item ItemNeutronUpgradeProd = new ItemNeutronUpgrade("ItemNeutronUpgradeProd", "ItemNeutronUpgradeProd");
+    public static Item ItemNeutronUpgradeIng = new ItemNeutronUpgrade("ItemNeutronUpgradeIng", "ItemNeutronUpgradeIng");
+
+}
